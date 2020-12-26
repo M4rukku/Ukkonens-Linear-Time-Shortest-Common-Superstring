c8947a680023e1bb26b6169de1a31c0f8a2b8991
M4rukku <57199511+M4rukku@users.noreply.github.com>
2020-12-26 10:22:43 +0100

    Added ClassTypeNodeFactory

---
+++ src/main/java/trie_nodes/ClassTypeNodeFactory.java
@@ -0,0 +1,50 @@
+package trie_nodes;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import trie_nodes.ACTrieNode;
+import alphabet.LanguageParameters;
+import trie_nodes.AbstractACNodeFactory;
+
+//* A quick and dirty pythonesque duck typing factory -
+// It will construct something that extends AhoCorasiclTrieNode, if it implements the correct constructor interface
+// language.LanguageParameters, boolean, char
+// */
+public class ClassTypeNodeFactory<T extends ACTrieNode> implements
+    AbstractACNodeFactory<T> {
+
+  private Constructor<T> nodeConstructor;
+
+  public ClassTypeNodeFactory(Class<T> nodeType) {
+    Class<?>[] classes = new Class[3];
+    classes[0] = LanguageParameters.class;
+    classes[1] = boolean.class;
+    classes[2] = char.class;
+    try {
+      nodeConstructor = nodeType.getDeclaredConstructor(classes);
+    } catch (NoSuchMethodException e) {
+      throw new RuntimeException(
+          "The class passed to NodeFactory does not implement the right constructor protocol", e);
+    }
+  }
+
+  /**
+   * A simple factory method that takes a set of default parameters and then returns a new Suffix
+   * Trie Node based on it.
+   *
+   * @param parameters  The {@link LanguageParameters} that define our underlying Alphabet
+   * @param isEndOfWord Indicates whether this Suffix Trie Node represents the end of a word
+   * @param parentChar  Character - that tells us the character of our parent
+   * @return Returns a new Trie Node
+   */
+  @Override
+  public T createFromDefaultValues(
+      LanguageParameters parameters, boolean isEndOfWord, Character parentChar) {
+    try {
+      return nodeConstructor.newInstance(parameters, isEndOfWord, parentChar);
+    } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+      throw new RuntimeException(
+          "Something went wrong when creating an AhoCorasickTreeNode in the NodeFactory!", e);
+    }
+  }
+}
